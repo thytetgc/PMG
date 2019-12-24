023432bd9e012f79b51de91d2c7d1910634ba36f
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2019-12-24 14:20:23 -0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo [`date +'%d.%m.%Y %H:%M'`] Iniciando Backup ...
+
+echo [`date +'%d.%m.%Y %H:%M'`] Exportar configuração atual [1/4] ...
+rm /var/lib/pmg/backup/*
+pmgbackup backup
+
+# Diretórios para fazer backup, /etc /root /var
+source='/etc /lib/systemd/system /root /usr/local /var/dcc /var/lib/pmg/backup /var/log'
+
+# Diretório de backup, /backup/node
+backup=/backup/node
+
+# Definição de Variavel
+datum=$(date +'%Y%m%d')
+dateiname=$backup/backup$datum.tar
+
+# -----------------------------------------------------
+function f_delFiles()
+# -----------------------------------------------------
+# $1 Diretório de backup
+{
+  loeschdatum=$(date --date='7 days ago' +'%Y%m%d')
+  rm $1/backup$loeschdatum.tar
+}
+
+echo [`date +'%d.%m.%Y %H:%M'`] Exclundo arquivos em $backup, com mais de 7 dias [2/4] ...
+f_delFiles $backup
+
+echo [`date +'%d.%m.%Y %H:%M'`] Salve $source em $dateiname [3/4] ...
+tar Pcf $dateiname $source
+
+echo [`date +'%d.%m.%Y %H:%M'`] Sincronizar com armazenamento online [4/4] ...
+bash /root/scripts/upload.sh
+
+echo [`date +'%d.%m.%Y %H:%M'`] Pronto!
