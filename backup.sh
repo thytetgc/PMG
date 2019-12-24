9ff5197da2c293d3537012038b5ae4ca706c1e57
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2019-12-24 16:23:03 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,10 +2,12 @@
 clear
 
 echo [`date +'%d.%m.%Y %H:%M'`] Iniciando Backup ...
+	 echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Exportar configuração atual [1/4] ...
 rm /var/lib/pmg/backup/*
 pmgbackup backup
+	 echo "+-------------------------------------------------+OK"
 
 # Diretórios para fazer backup, /etc /root /var
 source='/etc /Scripts /lib/systemd/system /root /usr/local /var/dcc /var/lib/pmg/ /var/log /opt'
@@ -30,11 +32,15 @@ function f_delFiles()
 
 echo [`date +'%d.%m.%Y %H:%M'`] Exclundo arquivos em $backup, com mais de 7 dias [2/4] ...
 f_delFiles $backup
+	 echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Salve $source em $dateiname [3/4] ...
 tar Pcf $dateiname $source
+	 echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Sincronizar com armazenamento online [4/4] ...
 bash /Scripts/upload.sh
+	 echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Pronto!
+	 echo "+-------------------------------------------------+OK"
