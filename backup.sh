1165932e300d65ea5f14925b8ee8741d3c4d78a7
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2019-12-29 17:35:07 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,16 +10,23 @@ pmgbackup backup
 	 echo "+-------------------------------------------------+OK"
 
 # Diretórios para fazer backup, /etc /root /var
-source='/etc /Scripts /lib/systemd/system /root /usr/local /var/dcc /var/lib/pmg/ /var/log /opt'
+source='/etc /Scripts /lib/systemd/system /root /usr/local /var/dcc /var/lib/pmg/ /opt'
 
 # Diretório de backup, /Backup
 #backup=/Backup/"`hostname`"
 backup=/Backup
 mkdir -p $backup
 
+# Senha para compactação
+senha='q|?a)N2#8d^JS^lm2EF29g*D$l#}aIu('
+
+# Pasta para Excluir do backup
+#exclude=''
+
 # Definição de Variavel
 datum=$(date +'%Y%m%d')
-dateiname=$backup/"`hostname -a`"-backup$datum.tar
+dateinametar=$backup/"`hostname -a`"-backup$datum.tar
+dateinamezip=$backup/"`hostname -a`"-backup$datum.zip
 
 # -----------------------------------------------------
 function f_delFiles()
@@ -27,21 +34,22 @@ function f_delFiles()
 # $1 Diretório de backup
 {
   loeschdatum=$(date --date='7 days ago' +'%Y%m%d')
-  rm $1/"`hostname -a`"-backup$loeschdatum.tar
+  rm $1/"`hostname -a`"-backup$loeschdatum.*
 }
 
 echo [`date +'%d.%m.%Y %H:%M'`] Exclundo arquivos em $backup, com mais de 7 dias [2/4] ...
 f_delFiles $backup
-	 echo "+-------------------------------------------------+OK"
+         echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Salve $source em $dateiname [3/4] ...
-tar Pcf $dateiname $source
-	 echo "+-------------------------------------------------+OK"
+tar -Pcf $dateinametar $source
+zip -P $senha -r $dateinamezip $dateinametar
+rm $dateinametar
+         echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Sincronizar com armazenamento online [4/4] ...
 bash /Scripts/upload.sh
-	 echo "+-------------------------------------------------+OK"
+         echo "+-------------------------------------------------+OK"
 
 echo [`date +'%d.%m.%Y %H:%M'`] Pronto!
-	 echo "+-------------------------------------------------+OK"
-
+         echo "+-------------------------------------------------+OK"
